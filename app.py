4b51c546cbb29cba616a9ebf89f09fa11d4af825
CorollaD <1976007504@qq.com>
2020-03-06 16:17:59 +0800

    sixth commit

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ while True:
                                 password = getpass("\n\t密码:")
                                 repassword = getpass("\n\t重复密码:")
                                 if password != repassword:
-                                    print("\n\t两次密码不一致(3s自动返回)")
+                                    print(Fore.LIGHTBLUE_EX, "\n\t两次密码不一致(3s自动返回)")
                                     time.sleep(3)
                                     continue
                                 email = input("\n\t邮箱:")
@@ -141,15 +141,96 @@ while True:
                                 __user_service.insert(username, password, email, role_id)
                                 print("\n\t保存成功(3s自动返回)")
                                 time.sleep(3)
-                                continue
-
-
-                    elif opt == "back":
-                        break
-                    elif opt == "exit":
-                        sys.exit(0)
-        else:
-            print("\n\t登录失败(3s自动返回)")
-            time.sleep(3)
+                                # continue
+                            elif opt=="2":
+                                page = 1
+                                while True:
+                                    os.system("clear")
+                                    count_page = __user_service.search_count_page()
+                                    result = __user_service.search_list(page)
+                                    for index in range(len(result)):
+                                        one = result[index]
+                                        print(Fore.LIGHTBLUE_EX, "\n\t%d\t%s\t%s" % (
+                                            index + 1, one[1], one[2]))
+                                    print(Fore.LIGHTBLUE_EX,
+                                          "\n\t---------------------")
+                                    print(Fore.LIGHTBLUE_EX, "\n\t%d/%d" %
+                                          (page, count_page))
+                                    print(Fore.LIGHTBLUE_EX,
+                                          "\n\t---------------------")
+                                    print(Fore.LIGHTRED_EX, "\n\tback.返回上一层")
+                                    print(Fore.LIGHTRED_EX, "\n\tprev.上一页")
+                                    print(Fore.LIGHTRED_EX, "\n\tnext.下一页")
+                                    print(Style.RESET_ALL)
+                                    opt = input("\n\t输入操作编号:")
+                                    if opt == "back":
+                                        break
+                                    elif opt == "prev" and page > 1:
+                                        page -= 1
+                                    elif opt == "next" and page < count_page:
+                                        page += 1
+                                    elif int(opt) >= 1 and int(opt) <= 10:
+                                        os.system("clear")
+                                        user_id=result[int(opt)-1][0]
+                                        username = input("\n\t新用户名:")
+                                        password = getpass("\n\t新密码:")
+                                        repassword = getpass("\n\t重复密码:")
+                                        if password != repassword:
+                                            print(Fore.LIGHTBLUE_EX, "\n\t两次密码不一致(3s自动返回)")
+                                            time.sleep(3)
+                                            break
+                                        email = input("\n\t新邮箱:")
+                                        result = __role_service.search_list()
+                                        for index in range(len(result)):
+                                            one = result[index]
+                                            print(Fore.LIGHTBLUE_EX, "\n\t%d.%s" % (index + 1, one[1]))
+                                        print(Style.RESET_ALL)
+                                        opt = input("\n\t角色编号:")
+                                        role_id = result[int(opt) - 1][0]
+                                        opt = input("\n\t是否保存(Y/N)")
+                                        if opt =="Y" or opt=="y":
+                                            __user_service.update(user_id, username,password,email, role_id)
+                                            print("\n\t保存成功(3s自动返回)")
+                                            time.sleep(3)
+                            elif opt=="3":
+                                page = 1
+                                while True:
+                                    os.system("clear")
+                                    count_page = __user_service.search_count_page()
+                                    result = __user_service.search_list(page)
+                                    for index in range(len(result)):
+                                        one = result[index]
+                                        print(Fore.LIGHTBLUE_EX, "\n\t%d\t%s\t%s" % (
+                                            index + 1, one[1], one[2]))
+                                    print(Fore.LIGHTBLUE_EX,
+                                          "\n\t---------------------")
+                                    print(Fore.LIGHTBLUE_EX, "\n\t%d/%d" %
+                                          (page, count_page))
+                                    print(Fore.LIGHTBLUE_EX,
+                                          "\n\t---------------------")
+                                    print(Fore.LIGHTRED_EX, "\n\tback.返回上一层")
+                                    print(Fore.LIGHTRED_EX, "\n\tprev.上一页")
+                                    print(Fore.LIGHTRED_EX, "\n\tnext.下一页")
+                                    print(Style.RESET_ALL)
+                                    opt = input("\n\t输入操作编号:")
+                                    if opt == "back":
+                                        break
+                                    elif opt == "prev" and page > 1:
+                                        page -= 1
+                                    elif opt == "next" and page < count_page:
+                                        page += 1
+                                    elif int(opt) >= 1 and int(opt) <= 10:
+                                        os.system("clear")
+                                        user_id=result[int(opt)-1][0]
+                                        __user_service.delete_by_id(user_id)
+                                        print("\n\t保存成功(3s自动返回)")
+                                        time.sleep(3)
+                            elif opt == "back":
+                                break
+                            elif opt == "exit":
+                                sys.exit(0)
+                            else:
+                                print("\n\t登录失败(3s自动返回)")
+                                time.sleep(3)
     elif opt == "2":
         sys.exit(0)
--- db/user_dao.py
+++ db/user_dao.py
@@ -49,3 +49,74 @@ class UserDao:
         finally:
             if "con" in dir():
                 con.close()
+
+    # 查询用户总页数
+    def search_count_page(self):
+        try:
+            con = pool.get_connection()
+            cursor = con.cursor()
+            sql = "SELECT CEIL(COUNT(*)/10) FROM t_user"
+            cursor.execute(sql)
+            count_page = cursor.fetchone()[0]
+            return count_page
+        except Exception as e:
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
+
+    # 查询用户的分页记录
+    def search_list(self, page):
+        try:
+            con = pool.get_connection()
+            cursor = con.cursor()
+            sql = "SELECT u.id,u.username, r.role" \
+                  "  FROM t_user u JOIN t_role r " \
+                  "ON u.role_id=r.id" \
+                  "ORDER BY u.id" \
+                  "LIMIT %s,%s"
+            cursor.execute(sql, (page-1)*10, 10)
+            result = cursor.fetchone()[0]
+            return result
+        except Exception as e:
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
+
+    # 修改用户信息
+    def update(self, id, username, password, email, role_id):
+        try:
+            con = pool.get_connection()
+            con.start_transaction()
+            cursor = con.cursor()
+            sql = "UPDATE t_user SET username=%s," \
+                  "password=HEX(AES_ENCRYPT(%s, 'HelloWorld'))," \
+                  "email=%s, role_id=%s" \
+                  "WHERE id=%s"
+            cursor.execute(sql, (username, password, email, role_id, id))
+            con.commit()
+        except Exception as e:
+            if "con" in dir():
+                con.rollback()
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
+
+    # 删除用户
+    def delete_by_id(self, id):
+        try:
+            con = pool.get_connection()
+            con.start_transaction()
+            cursor = con.cursor()
+            sql = "DELETE FROM t_user WHERE id=%s"
+            cursor.execute(sql, [id])
+            con.commit()
+        except Exception as e:
+            if "con" in dir():
+                con.rollback()
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
